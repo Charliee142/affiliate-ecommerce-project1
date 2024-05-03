809b4fd10c22e2f733d788748cd894d54f707a13
Welcome Charles <charlespeter142@gmail.com>
2024-05-03 20:49:15 +0100

    initial commit

---
+++ main/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+
+
+app_name = "main"   
+
+
+urlpatterns = [
+    path("", views.homepage, name="homepage"),
+    path("products/", views.products, name = "products"),
+    path("blog/", views.blog, name ="blog"),
+    path("blog/<tag_page>/", views.blog, name ="blog"),
+    path("<article_page>", views.article, name = "article"),
+    path("user/", views.userpage, name = "userpage"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login_request, name ="login"),
+    path("logout/", views.logout_request, name= "logout"),
+   
+]
